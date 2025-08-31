7a1b919272f4f98b61375232f283183bd16f30e6
aristeap <aristeapapaspyroy@gmail.com>
2025-08-31 17:22:01 +0300

    Adapted the video-parser.service.ts, so it could look like the other angularjs services in hopes to fix the error in the console

--- src/app/services/video-parser.service.ts
+++ src/app/services/video-parser.service.ts
@@ -1,37 +1,44 @@
 import * as angular from 'angular';
 
-angular.module('emuwebApp')
-.service('VideoParserService', function($q, $window) {
+class VideoParserService {
+    private $q;
+    private $window;
 
-  this.parseVideoAudioBuf = function(buf) {
-    try {
-      // Use the OfflineAudioContext (or webkitOfflineAudioContext) from $window.
-      const AudioContext = $window.OfflineAudioContext || $window.webkitOfflineAudioContext;
-      // Assume stereo (2 channels) and a sample rate of 44100 Hz.
-      const numChannels = 2;
-      const sampleRate = 44100;
-      // Calculate the total samples; assuming 16 bits per sample (2 bytes per sample)
-      const totalSamples = buf.byteLength / 2 / numChannels;
-      const offlineCtx = new AudioContext(numChannels, totalSamples, sampleRate);
-
-      let defer = $q.defer();
-      // Decode the audio data from the buffer
-      offlineCtx.decodeAudioData(buf,
-        function(decodedData) { 
-          defer.resolve(decodedData); 
-        },
-        function(error) { 
-          defer.reject(error); 
-        }
-      );
-      return defer.promise;
-    } catch (e) {
-      let err = {
-        status: { message: JSON.stringify(e, null, 4) }
-      };
-      let defer = $q.defer();
-      defer.reject(err);
-      return defer.promise;
+    constructor($q, $window) {
+        this.$q = $q;
+        this.$window = $window;
     }
-  };
-});
+
+    parseVideoAudioBuf(buf) {
+        try {
+            const AudioContext = this.$window.OfflineAudioContext || this.$window.webkitOfflineAudioContext;
+            const numChannels = 2;
+            const sampleRate = 44100;
+            const totalSamples = buf.byteLength / 2 / numChannels;
+            const offlineCtx = new AudioContext(numChannels, totalSamples, sampleRate);
+
+            let defer = this.$q.defer();
+            offlineCtx.decodeAudioData(buf,
+                function(decodedData) {
+                    defer.resolve(decodedData);
+                },
+                function(error) {
+                    defer.reject(error);
+                }
+            );
+            return defer.promise;
+        } catch (e) {
+            let err = {
+                status: {
+                    message: JSON.stringify(e, null, 4)
+                }
+            };
+            let defer = this.$q.defer();
+            defer.reject(err);
+            return defer.promise;
+        }
+    }
+}
+
+angular.module('emuwebApp')
+.service('VideoParserService', ['$q', '$window', VideoParserService]);
